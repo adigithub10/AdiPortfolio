8b67a651495cb7c84daf8d50708615220b263ef5
Aditi <aditisr1074@gmail.com>
2024-09-29 00:26:29 +0530

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://adigithub10.github.io',
+  // base :'/https://github.com/adigithub10/AdiPortfolio',
+
   integrations: [tailwind()]
 });

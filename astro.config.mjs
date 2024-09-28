e93a5a5a425ef7f3d5c757878c757b22ed11aa3a
Aditi <aditisr1074@gmail.com>
2024-09-29 00:52:01 +0530

    temp change in site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://adigithub10.github.io',
+  // site:'https://adigithub10.github.io',
+  site:'https://github.com/adigithub10/AdiPortfolio',
   // base :'/https://github.com/adigithub10/AdiPortfolio',
 
   integrations: [tailwind()]

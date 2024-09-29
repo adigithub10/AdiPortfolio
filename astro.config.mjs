dc7311dfaab261712dd08f0afaebb1ddc685760c
Yash Jawale <yash25.j@gmail.com>
2024-09-29 13:30:08 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
    site:'https://adigithub10.github.io',
-   base: 'AdiPortfolio',
+   base: '/AdiPortfolio',
   integrations: [tailwind()]
 });
